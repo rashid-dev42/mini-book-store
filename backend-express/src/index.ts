bf1e737ea1d4c4cc7d5f9b091c1b424ed8fae008
devCake1 <shipc4836@gmail.com>
2026-01-25 23:21:56 +0600

    add process.env.PORT to backend-express

--- backend-express/src/index.ts
+++ backend-express/src/index.ts
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 import fs from "fs";
 import { Book } from "./models/bookModel";
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // const upload = multer({ dest: './uploads/' });
 const storage = multer.diskStorage({
