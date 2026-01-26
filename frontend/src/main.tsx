221dc7cd915e28a9b7e93ac29e37b034853af237
devCake1 <shipc4836@gmail.com>
2026-01-27 00:41:04 +0600

    add live url of backend-graphql

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.tsx'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://mini-book-store-backend-graphql.onrender.com',
   cache: new InMemoryCache(),
 });
 
